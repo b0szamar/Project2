0c0b2e984bfc2c814a3706031bf6554c0d6fe8d3
2020b-Szabó Marcell <74899044+b0szamar@users.noreply.github.com>
2023-12-14 18:54:00 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,77 +1,65 @@
-let szamok = [];
-function generateUniqueRandom(maxNr) {
-    let random = (Math.random() * maxNr).toFixed();
-    random = Number(random);
-    if(!szamok.includes(random)) {
-        szamok.push(random);
-        
-        return random;
+const emojis = ['💗', '😳', '😴', '😍', '🤑', '💩', '💀', '🤡'];
+const cards = emojis.concat(emojis);
+let flippedCards = [];
+let matchedCards = [];
+function shuffle(lista) {
+    for (let i = lista.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [lista[i], lista[j]] = [lista[j], lista[i]];
+    }
+}
+function createCard(emoji) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.emoji = emoji;
+    const emojiContainer = document.createElement('div');
+    emojiContainer.classList.add('emoji');
+    emojiContainer.textContent = emoji;
+    card.appendChild(emojiContainer);
+    card.addEventListener('click', flipCard);
+    return card;
+}
+function createBoard() {
+    shuffle(cards);
+    const gameContainer = document.querySelector('.memory-game');
+    cards.forEach(emoji => {
+        const card = createCard(emoji);
+        gameContainer.appendChild(card);
+    });
+}
+function flipCard() {
+    if (flippedCards.length < 2 && !this.classList.contains('flipped') && flippedCards.indexOf(this) === -1) {
+        this.classList.add('flipped');
+        flippedCards.push(this);
+        if (flippedCards.length === 2) {
+            setTimeout(checkMatch, 2000);
+        }
+    }
+}
+let tries=0;
+let ossz=0;
+function checkMatch() {
+    const [card1, card2] = flippedCards;
+    const emoji1 = card1.dataset.emoji;
+    const emoji2 = card2.dataset.emoji;
+    if (emoji1 === emoji2) {
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        matchedCards.push(card1, card2);
+        ossz=ossz+1;
+        if (matchedCards.length === cards.length) {
+            alert('Gratulálok! Összesen '+ossz+' probálkozásba telt megcsinálni!');
+            setTimeout(5000);
+            location.reload()
+        }
     } else {
-         return  generateUniqueRandom(maxNr);
+        card1.classList.remove('flipped');
+        card2.classList.remove('flipped');
+        tries=tries+1;
+        ossz=ossz+1;
     }
+    flippedCards = [];
+    document.getElementById('probak').innerHTML = "A sikertelen tippek száma: "+tries;
 }
-let szamok2=[];
-function egyenlo() {
-    for (let i = 0; i < szamok.length; i++) {
-        szamok2[i]=szamok[i];
-    }
-    
-    
-}
-function shuffle(szamok2) {
-    let currentIndex = szamok2.length,  randomIndex;
-    while (currentIndex > 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      [szamok2[currentIndex], szamok2[randomIndex]] = [szamok2[randomIndex], szamok2[currentIndex]];
-    }
-  
-    return szamok2;
-  }
-  
-function general() {
-        document.getElementById("szam1").innerHTML =szamok[0];
-        document.getElementById("szam1b").innerHTML =szamok2[0];
-        document.getElementById("szam2").innerHTML =szamok[1];
-        document.getElementById("szam2b").innerHTML =szamok2[1];
-        document.getElementById("szam3").innerHTML =szamok[2];
-        document.getElementById("szam3b").innerHTML =szamok2[2];
-        document.getElementById("szam4").innerHTML =szamok[3];
-        document.getElementById("szam4b").innerHTML =szamok2[3];
-        document.getElementById("szam5").innerHTML =szamok[4];
-        document.getElementById("szam5b").innerHTML =szamok2[4];
-        document.getElementById("szam6").innerHTML =szamok[5];
-        document.getElementById("szam6b").innerHTML =szamok2[5];
-        document.getElementById("szam7").innerHTML =szamok[6];
-        document.getElementById("szam7b").innerHTML =szamok2[6];
-        document.getElementById("szam8").innerHTML =szamok[7];
-        document.getElementById("szam8b").innerHTML =szamok2[7];
-
-        
-}
-console.log(generateUniqueRandom(100));
-console.log(generateUniqueRandom(100));
-console.log(generateUniqueRandom(100));
-console.log(generateUniqueRandom(100));
-console.log(generateUniqueRandom(100));
-console.log(generateUniqueRandom(100));
-console.log(generateUniqueRandom(100));
-console.log(generateUniqueRandom(100));
-console.log('Unique random numbers:' ,szamok);
-function szam1show(){document.getElementById("szam1").style.visibility = "visible";}
-function szam1bshow(){document.getElementById("szam1b").style.visibility = "visible";}
-function szam2show(){document.getElementById("szam2").style.visibility = "visible";}
-function szam2bshow(){document.getElementById("szam2b").style.visibility = "visible";}
-function szam3show(){document.getElementById("szam3").style.visibility = "visible";}
-function szam3bshow(){document.getElementById("szam3b").style.visibility = "visible";}
-function szam4show(){document.getElementById("szam4").style.visibility = "visible";}
-function szam4bshow(){document.getElementById("szam4b").style.visibility = "visible";}
-function szam5show(){document.getElementById("szam5").style.visibility = "visible";}
-function szam5bshow(){document.getElementById("szam5b").style.visibility = "visible";}
-function szam6show(){document.getElementById("szam6").style.visibility = "visible";}
-function szam6bshow(){document.getElementById("szam6b").style.visibility = "visible";}
-function szam7show(){document.getElementById("szam7").style.visibility = "visible";}
-function szam7bshow(){document.getElementById("szam7b").style.visibility = "visible";}
-function szam8show(){document.getElementById("szam8").style.visibility = "visible";}
-function szam8bshow(){document.getElementById("szam8b").style.visibility = "visible";}
+document.getElementById('probak').innerHTML = "A sikertelen tippek száma: ";
+createBoard();
